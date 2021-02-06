193b720b7c562d4dfe5d7b247457ff6dff0f9ad1
markjessell <markjessell@users.noreply.github.com>
2021-02-06 09:25:29 +0800

    Add noddyverse version of source code for noddy

---
+++ src/noddy_mingw.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : noddy_mingw.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int xmain(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
